53db179f312aa2f677b855cd45ee27886eedb335
Andersondias-49 <carneiroanderson911@gmail.com>
2026-09-25 14:08:23 -0300

    Update Estoque + MySql.py
    Versão final usando MySql

--- golden era/Estoque + MySql.py
+++ golden era/Estoque + MySql.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from time import sleep
 conexao = mysql.connector.connect(
 host = '127.0.0.1',
 port = '3306',
@@ -18,6 +19,7 @@ def listar_produtos():
         print(f'Preço: {c[3]}')
         print(f'Quantidade: {c[4]}')
         print()
+        sleep(1)
 
 
 def cadastrar_produto():
@@ -28,7 +30,8 @@ def cadastrar_produto():
     sql = """ insert into produtos (nome, categoria, preco, quantidade) values (%s, %s, %s, %s)"""
     cursor.execute(sql,(nome,categoria,preco,quantidade))
     conexao.commit()
-    print(f'{nome} CADASTRADO COM SUCESSO!')
+    print(f'{nome} Cadastrado com sucesso!')
+    sleep(1)
 
 
 def buscar_produto():
@@ -40,29 +43,57 @@ def buscar_produto():
         print('ID não encontrado!')
     else:
         print(f'ID: {p[0]}')
-        print(f'Categoria: {p[1]}')
-        print(f'Preço: {p[2]}')
-        print(f'Quantidade: {p[3]}')
-
+        print(f'Nome: {p[1]}')
+        print(f'Categoria: {p[2]}')
+        print(f'Preço: {p[3]}')
+        print(f'Quantidade: {p[4]}')
+        sleep(1)
 
 def alterar_preco():
     idp = int(input('Qual o id do produto na qual você deseja alterar o preço? '))
     novopreco = float(input('Qual será o novo preço? '))
-    sql = """ update produtos set preco = (%s) where id = (%s)"""
+    sql = """ update produtos set preco = (%s) where id = %s"""
     cursor.execute(sql,(novopreco,idp))
     if cursor.rowcount == 0:
         print('O ID inserido não foi encontrado!')
     else:
         print('Preço alterado com sucesso!')
     conexao.commit()
+    sleep(1)
 
 
 def deletar_linha():
     delete = int(input('Qual id do produto que você deseja Excluir? '))
     sql = """delete from produtos where id = %s"""
     cursor.execute(sql,(delete,))
+    c = cursor.rowcount
+    if c == 1:
+        print('Linha deletada com sucesso!')
+    else:
+        print('ID não encontrado!')
     conexao.commit()
-    print('OK')
+    sleep(1)
 
 
-listar_produtos()
+while True:
+    print('=== ESTOQUE ===\n'
+          '1 - Cadastrar Produto\n'
+          '2 - Listar Produto\n'
+          '3 - Buscar Produto\n'
+          '4 - Alterar Preço\n'
+          '5 - Excluir Produto\n'
+          '6 - Sair')
+    resposta = int(input('O que você deseja fazer? '))
+    if resposta == 1:
+        cadastrar_produto()
+    elif resposta == 2:
+        listar_produtos()
+    elif resposta == 3:
+        buscar_produto()
+    elif resposta == 4:
+        alterar_preco()
+    elif resposta == 5:
+        deletar_linha()
+    elif resposta == 6:
+        break
+print('=== SISTEMA ENCERRADO ===')
